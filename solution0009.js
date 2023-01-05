b79db9a8a8347e2f730e9e9bb8a18f844bf2de57
Esterjudith <106325339+Esterjudith@users.noreply.github.com>
2023-01-04 20:05:40 -0700

    fibonacci memoized

---
+++ solution0009.js
@@ -0,0 +1,16 @@
+// fibonacci memoized solution
+
+var memo = {}
+
+var fib = function (n) {
+    if (n === 0 || n === 1) {
+        return n
+    } else if (memo[n]) {
+        return memo[n]
+    } else {
+        var result = fib(n - 1) + fib(n - 2)
+        memo[n] = result
+        return result
+    }
+}
+console.log(fib(5))
