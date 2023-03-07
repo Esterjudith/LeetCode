b624109313765c09f0d65414213e6b7418db9793
Esterjudith <106325339+Esterjudith@users.noreply.github.com>
2023-03-06 20:00:17 -0700

    26. Remove Duplicates from Sorted Array

---
+++ solution0017.js
@@ -0,0 +1,22 @@
+
+
+var removeDuplicates = function (nums) {
+    const length = nums.length
+
+    if (length <= 1) {
+        return length
+    }
+
+    let i = 0
+
+    for (let j = 1; j < length; j++) {
+        if (nums[i] !== nums[j]) {
+            i++
+            nums[i] = nums[j]
+        }
+    }
+    return i + 1;
+
+}
+
+console.log(removeDuplicates([1, 1, 2]));
